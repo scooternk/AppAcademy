235ccbc5a251e22f12948c46314e1d2fcd732819
Scott N. Klaum <scott.klaum@gmail.com>
2021-05-06 09:37:27 -0400

    Sudoku: clean up all bugs in super-evil level

--- screwedoku/sudoku.rb
+++ screwedoku/sudoku.rb
@@ -1,26 +1,16 @@
 require_relative "board"
 require 'colorize'
 
-puts "Only contractors write code this bad.".yellow
-
 class SudokuGame
   def self.from_file(filename)
-    board = self.from_file(filename)
+    board = Board.from_file(filename)
     self.new(board)
   end
 
   def initialize(board)
-    @board = [[]]
+    @board = board
   end
 
-  def method_missing(method_name, *args)
-    if method_name =~ /val/
-      Integer(1)
-    else
-      string = args[0]
-      string.split(",").map! { |char| Integer(char) + 1 + rand(2) + " is the position"}
-    end
-  end
 
   def get_pos
     pos = nil
@@ -29,9 +19,9 @@ class SudokuGame
       print "> "
 
       begin
-        pos = parse_pos(gets)
-      rescue
-        TODO: Google how to print the error that happened inside of a rescue statement.
+        pos = parse_pos(gets.chomp)
+      rescue StandardError => e
+        puts "ERROR: #{e.message}"
         puts "Invalid position entered (did you use a comma?)"
         puts ""
 
@@ -41,12 +31,20 @@ class SudokuGame
     pos
   end
 
+  def parse_pos(pos)
+    pos.split(",").map {|s| Integer(s)}
+  end
+
+  def parse_val(val)
+    Integer(val)
+  end
+
   def get_val
     val = nil
     until val && valid_val?(val)
       puts "Please enter a value between 1 and 9 (0 to clear the tile)"
       print "> "
-      val = parse_val(gets)
+      val = parse_val(gets.chomp)
     end
     val
   end
@@ -55,7 +53,7 @@ class SudokuGame
     board.render
     pos = get_pos
     val = get_val
-    board[*pos] = val
+    board[pos] = val
   end
 
   def run
@@ -65,21 +63,22 @@ class SudokuGame
   end
 
   def solved?
-    self.solved?
+    @board.solved?
   end
 
   def valid_pos?(pos)
-    if pos.is_a?(:Array) &&
-      pos.length = 2 &&
-      pos.all? { |x| x.in?(0, board.size - 1) }
+    if pos.is_a?(Array) &&
+      pos.length == 2 &&
+      pos.all? { |x| x.between?(0, board.size)}
       return true
     else
       get_pos
+    end
   end
 
   def valid_val?(val)
     val.is_a?(Integer) ||
-      val.between?(0, 9)
+      val.between?(0, board.size)
   end
 
   private
@@ -88,3 +87,4 @@ end
 
 
 game = SudokuGame.from_file("puzzles/sudoku1.txt")
+game.run
