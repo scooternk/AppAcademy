13bc1c4d7d9f5291e535eb6d2084ad2d9fefd013
Scott N. Klaum <scott.klaum@gmail.com>
2021-08-31 11:28:36 -0400

    RSpec Homework
    Rename mock
    Test #mix! with more ingredients
    Test #serve with exact string expected

--- rspec_hw/spec/dessert_spec.rb
+++ rspec_hw/spec/dessert_spec.rb
@@ -7,53 +7,57 @@ Instructions: implement all of the pending specs (the `it` statements without bl
 
 describe Dessert do
   let(:chef) { double("chef") }
-  subject(:dessert) {Dessert.new("cookie", 10, chef)}
+  subject(:cookie) {Dessert.new("cookie", 10, chef)}
 
   describe "#initialize" do
     it "sets a type" do
-      expect(dessert.type).to eq("cookie")
+      expect(cookie.type).to eq("cookie")
     end
 
     it "sets a quantity" do
-      expect(dessert.quantity).to eq(10)
+      expect(cookie.quantity).to eq(10)
     end
 
     it "starts ingredients as an empty array" do
-      expect(dessert.ingredients).to be_empty
+      expect(cookie.ingredients).to be_empty
     end
 
     it "raises an argument error when given a non-integer quantity" do
-      expect {Dessert.new("cake", "big", "Garten")}.to raise_error(ArgumentError)
+      expect {Dessert.new("cake", "big", chef)}.to raise_error(ArgumentError)
     end
 
   end
 
   describe "#add_ingredient" do
     it "adds an ingredient to the ingredients array" do
-      expect(dessert.ingredients).to be_empty
-      dessert.add_ingredient("chocolate chips")
-      expect(dessert.ingredients).to include("chocolate chips")
+      cookie.add_ingredient("chocolate chips")
+      expect(cookie.ingredients).to include("chocolate chips")
     end
   end
 
   describe "#mix!" do
     it "shuffles the ingredient array" do
-      dessert.add_ingredient("butter")
-      dessert.add_ingredient("sugar")
-      expect(dessert.ingredients).to eq(["butter","sugar"])
-      dessert.mix!
-      expect(dessert.ingredients).to contain_exactly("butter","sugar")
+      ingredients = ["chocolate", "flour", "egg", "sugar", "butter"]
+
+      ingredients.each do |ingredient|
+        cookie.add_ingredient(ingredient)
+      end
+
+      expect(cookie.ingredients).to eq(ingredients)
+      cookie.mix!
+      expect(cookie.ingredients).not_to eq(ingredients) # confirm shuffle
+      expect(cookie.ingredients.sort).to eq(ingredients.sort) # confirm arrays still have same elements
     end
   end
 
   describe "#eat" do
     it "subtracts an amount from the quantity" do
-      dessert.eat(2)
-      expect(dessert.quantity).to eq 8
+      cookie.eat(2)
+      expect(cookie.quantity).to eq 8
     end
 
     it "raises an error if the amount is greater than the quantity" do
-      expect {dessert.eat(20)}.to raise_error
+      expect {cookie.eat(20)}.to raise_error("not enough left!")
     end
   end
 
@@ -61,14 +65,14 @@ describe Dessert do
     it "contains the titleized version of the chef's name" do
       allow(chef).to receive(:titleize).and_return("Chef Ramsey the Great Baker")
       expect(chef).to receive(:titleize)
-      expect(dessert.serve).to include("Chef Ramsey the Great Baker")
+      expect(cookie.serve).to eq("Chef Ramsey the Great Baker has made 10 cookies!")
     end
   end
 
   describe "#make_more" do
     it "calls bake on the dessert's chef with the dessert passed in" do
-      expect(chef).to receive(:bake).with(dessert)
-      dessert.make_more
+      expect(chef).to receive(:bake).with(cookie)
+      cookie.make_more
     end
   end
 end
